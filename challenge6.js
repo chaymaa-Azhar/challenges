0d15c1eef898ac500940c32ff529a8815c388295
chaymaa <azharchaymaa@gmail.com>
2026-09-17 11:13:59 +0200

    challenge6

---
+++ challenge6.js
@@ -0,0 +1,9 @@
+const prompt =require(`prompt-sync`)();
+let a =Number(prompt(`Entrer nombre a :`));
+let b =Number(prompt(`Entrer nombre b :`));
+console.log(`la somme est  ${a+b}`);
+console.log(`la différence est  ${a-b}`);
+console.log(`la somme est  ${a+b}`);
+console.log(`le produit est  ${a*b}`);
+console.log(`la différence est  ${a-b}`);
+console.log(`le quotient est  ${a/b}`);
