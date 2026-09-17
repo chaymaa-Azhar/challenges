15229474bafa67e05a62131b0a1d981ce269933d
chaymaa <azharchaymaa@gmail.com>
2026-09-17 14:14:49 +0200

    modiffier un erreur

--- challenge6.js
+++ challenge6.js
@@ -3,7 +3,5 @@ let a =Number(prompt(`Entrer nombre a :`));
 let b =Number(prompt(`Entrer nombre b :`));
 console.log(`la somme est  ${a+b}`);
 console.log(`la différence est  ${a-b}`);
-console.log(`la somme est  ${a+b}`);
 console.log(`le produit est  ${a*b}`);
-console.log(`la différence est  ${a-b}`);
 console.log(`le quotient est  ${a/b}`);
