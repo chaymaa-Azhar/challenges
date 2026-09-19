48d714c151b21a5edfbcbb60ae46edbb95edf05f
chaymaa <azharchaymaa@gmail.com>
2026-09-19 17:05:27 +0200

    add challenges

---
+++ les tableaux/challenge8.js
@@ -0,0 +1,8 @@
+const prompt=require('prompt-sync')();
+let tableau=[1,2,3,4,5];
+let C_tableau =[];
+for(let i=0; i<5; i++){
+    C_tableau[i]=tableau[i]
+}
+console.log(tableau)
+console.log(C_tableau)
